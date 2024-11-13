4848e530ff4fac67abc66b509c031a47f13ffa04
Khoa Phan <116791213+Silnix@users.noreply.github.com>
2024-11-13 14:59:15 +0700

    Add files via upload

---
+++ Bai25.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao mot so nguyen duong n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 1)
+            {
+                Console.WriteLine(n + " khong phai la so nguyen to.");
+            }
+            else
+            {
+                bool isPrime = true;
+
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+                    if (n % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine(n + " la so nguyen to.");
+                }
+                else
+                {
+                    Console.WriteLine(n + " khong phai la so nguyen to.");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
