a73fd53ac4f7ab318c09f8d994e8031eefaad38b
Khoa Phan <116791213+Silnix@users.noreply.github.com>
2024-11-13 15:40:14 +0700

    Add files via upload

---
+++ Bai19.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap n: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            int i = 1;
+            int Dem = 0;
+
+            while (i <= n)
+            {
+                if (n % i == 0)
+                    ++Dem;
+                ++i;
+            }
+
+            Console.WriteLine("So uoc so cua {0} la {1}", n, Dem);
+            Console.ReadLine();
+        }
+    }
+}
