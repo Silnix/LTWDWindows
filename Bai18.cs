d302ca5d1952fa6b9d2f385f18a27673ffd735ee
Khoa Phan <116791213+Silnix@users.noreply.github.com>
2024-11-13 15:38:10 +0700

    Add files via upload

---
+++ Bai18.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap n: ");
+            n = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Cac uoc so cua n: ");
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    Console.WriteLine("" + i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
