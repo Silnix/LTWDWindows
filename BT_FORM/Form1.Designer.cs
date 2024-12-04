d7511964a60a662030c34dbecea65125bac55022
Khoa Phan <116791213+Silnix@users.noreply.github.com>
2024-12-04 13:39:25 +0700

    Add files via upload

---
+++ BT_FORM/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace BT_FORM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.txtMaKH = new System.Windows.Forms.TextBox();
+            this.txtHoten = new System.Windows.Forms.TextBox();
+            this.GioiTinh1 = new System.Windows.Forms.RadioButton();
+            this.GioiTinh2 = new System.Windows.Forms.RadioButton();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.NamSinh = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 273);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(709, 108);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // txtMaKH
+            // 
+            this.txtMaKH.Location = new System.Drawing.Point(68, 36);
+            this.txtMaKH.Name = "txtMaKH";
+            this.txtMaKH.Size = new System.Drawing.Size(100, 20);
+            this.txtMaKH.TabIndex = 1;
+            // 
+            // txtHoten
+            // 
+            this.txtHoten.Location = new System.Drawing.Point(68, 86);
+            this.txtHoten.Name = "txtHoten";
+            this.txtHoten.Size = new System.Drawing.Size(100, 20);
+            this.txtHoten.TabIndex = 2;
+            this.txtHoten.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // GioiTinh1
+            // 
+            this.GioiTinh1.AutoSize = true;
+            this.GioiTinh1.Location = new System.Drawing.Point(68, 124);
+            this.GioiTinh1.Name = "GioiTinh1";
+            this.GioiTinh1.Size = new System.Drawing.Size(45, 17);
+            this.GioiTinh1.TabIndex = 5;
+            this.GioiTinh1.TabStop = true;
+            this.GioiTinh1.Text = "nam";
+            this.GioiTinh1.UseVisualStyleBackColor = true;
+            // 
+            // GioiTinh2
+            // 
+            this.GioiTinh2.AutoSize = true;
+            this.GioiTinh2.Location = new System.Drawing.Point(119, 124);
+            this.GioiTinh2.Name = "GioiTinh2";
+            this.GioiTinh2.Size = new System.Drawing.Size(39, 17);
+            this.GioiTinh2.TabIndex = 6;
+            this.GioiTinh2.TabStop = true;
+            this.GioiTinh2.Text = "Nữ";
+            this.GioiTinh2.UseVisualStyleBackColor = true;
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(68, 150);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(100, 20);
+            this.txtSDT.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Mã KH";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Họ và Tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Giới tính";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "SDT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(343, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Năm sinh";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Địa chỉ";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(68, 191);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(100, 20);
+            this.txtDiaChi.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // NamSinh
+            // 
+            this.NamSinh.Location = new System.Drawing.Point(421, 22);
+            this.NamSinh.Name = "NamSinh";
+            this.NamSinh.Size = new System.Drawing.Size(200, 20);
+            this.NamSinh.TabIndex = 17;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Sửa ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(318, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 393);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.NamSinh);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.GioiTinh2);
+            this.Controls.Add(this.GioiTinh1);
+            this.Controls.Add(this.txtHoten);
+            this.Controls.Add(this.txtMaKH);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox txtMaKH;
+        private System.Windows.Forms.TextBox txtHoten;
+        private System.Windows.Forms.RadioButton GioiTinh1;
+        private System.Windows.Forms.RadioButton GioiTinh2;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker NamSinh;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ BT_FORM/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT_FORM
+{
+    public partial class Form1 : Form
+    {
+             
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.Columns.Add("Mã Khách Hàng", 100);
+            listView1.Columns.Add("Họ Tên", 150);
+            listView1.Columns.Add("Giới Tính", 100);
+            listView1.Columns.Add("Ngày Sinh", 100);
+            listView1.Columns.Add("Số Điện Thoại", 150);
+            listView1.Columns.Add("Địa Chỉ", 250);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Lấy dữ liệu từ các ô nhập liệu
+            string maKH = txtMaKH.Text.Trim(); // Mã khách hàng
+            string hoten = txtHoten.Text.Trim(); // Họ tên
+            string gioitinh = GioiTinh1.Checked ? "Nam" : "Nữ"; // Kiểm tra giới tính (Nam hay Nữ)
+            string sdt = txtSDT.Text.Trim(); // Số điện thoại
+            string diachi = txtDiaChi.Text.Trim(); // Địa chỉ
+
+            // Lấy ngày sinh từ DateTimePicker
+            string ngaysinh = NamSinh.Value.ToString("dd/MM/yyyy");  // Nếu bạn dùng DateTimePicker
+
+            // Kiểm tra xem có đủ thông tin không
+            if (string.IsNullOrEmpty(maKH) || string.IsNullOrEmpty(hoten) || string.IsNullOrEmpty(sdt)
+                || string.IsNullOrEmpty(diachi))
+            {
+                MessageBox.Show("Vui lòng nhập đủ thông tin", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Nếu thiếu thông tin, không thực hiện tiếp
+            }
+
+            // Tạo một item mới để thêm vào ListView
+            ListViewItem item = new ListViewItem(maKH);  // Mã khách hàng là phần tử chính
+            item.SubItems.Add(hoten); // Thêm họ tên
+            item.SubItems.Add(gioitinh); // Thêm giới tính
+            item.SubItems.Add(ngaysinh); // Thêm ngày sinh
+            item.SubItems.Add(sdt); // Thêm số điện thoại
+            item.SubItems.Add(diachi); // Thêm địa chỉ
+
+            // Kiểm tra listView1 trước khi thêm
+            if (listView1 != null)
+            {
+                listView1.Items.Add(item);  // Thêm item vào ListView
+            }
+            else
+            {
+                MessageBox.Show("ListView chưa được khởi tạo!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Xóa dữ liệu trong các ô nhập liệu sau khi thêm
+            txtMaKH.Clear();
+            txtHoten.Clear();
+            txtSDT.Clear();
+            txtDiaChi.Clear();
+
+            // Đặt lại giá trị của DateTimePicker và checkbox
+            NamSinh.Value = DateTime.Now;
+            GioiTinh1.Checked = false;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { 
+          if(listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("ListView chưa được khởi tạo!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            ListViewItem selecteditem = listView1.SelectedItems[0];
+            selecteditem.Text = txtMaKH.Text.Trim();
+            selecteditem.SubItems[1].Text = txtHoten.Text.Trim();
+            selecteditem.SubItems[2].Text = GioiTinh1.Checked ? "Nam" : "Nữ";
+
+            selecteditem.SubItems[3].Text = txtSDT.Text.Trim();
+
+        }
+    }
+}
+
